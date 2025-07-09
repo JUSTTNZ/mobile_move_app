5947e63d97cde9c40f63a70360a1a4fa64bbb8aa
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-07-09 15:38:27 +0100

    Implement multiple code changes to enhance functionality and improve performance

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import React from 'react'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
-export default function Index() {
+const Index = () => {
   return (
-    <View className="flex-1 bg-primary">
-      
+    <View className='flex-1 bg-primary relative'>
+      <Image source={images.bg} className='absolute w-full z-0' />
+
+      <ScrollView className='flex-1 px-5' showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: '100%' }}>
+        <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto'/>
+      </ScrollView>
     </View>
-  );
+  )
 }
+
+export default Index
+
+const styles = StyleSheet.create({})
