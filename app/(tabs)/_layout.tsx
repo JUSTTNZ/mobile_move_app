2889008bded8773954e8249bf886d907c89bd39a
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-07-09 07:04:22 +0100

    fix: update app icons and improve tab layout for better consistency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,33 +4,27 @@ import { Tabs } from 'expo-router'
 import { images } from '@/constants/images'; // Uncomment if you want to use expo-image for better performance
 import { icons } from '@/constants/icons';
 
-const TabIcon = ({focused, icon, title}: any) => {
+function TabIcon({ focused, icon, title }: any) {
   if (focused) {
     return (
-      <ImageBackground 
-                source={images.highlight}
-                className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'>
-                <Image 
-                  source={icon} 
-                  className='size-5' 
-                  tintColor="#151312"
-                />
-                <Text className='text-secondary text-base font-semibold'>{title}</Text>
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
       </ImageBackground>
-  )
+    );
+  }
 
-  }
-    return (
-      <View className='size=full justify-center items-center mt-4 rounded-full'>
-        <Image 
-          source={icon} 
-          className='size-5' 
-          tintColor="#A8B5DB"
-        />
-        <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
-      </View>
-    )
-  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+}
 
 const _Layout = () => {
   return (
