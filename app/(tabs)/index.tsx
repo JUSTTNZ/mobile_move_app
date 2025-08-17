bcb50089d653da5e73a2dccff4e44f2ef0d35686
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-08-17 17:20:03 +0100

    fixing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -28,7 +28,7 @@ const Index = () => {
         { moviesLoading ? (
           <ActivityIndicator 
             size='large' 
-            color='#fff' 
+            color='#fff'
             className='mt-10 self-center'
           />
         ) : moviesError ? (
